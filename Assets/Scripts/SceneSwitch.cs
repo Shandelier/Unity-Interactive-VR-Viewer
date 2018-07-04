cb24b70fdd6a8277b9f5af9a8715b22e0ee1ce59
Unknown <asmonix@gmail.com>
2018-07-04 19:33:38 +0200

    InitCommit

---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour {
+
+	public void OpenVenus()
+	{
+		SceneManager.LoadScene("SceneVenus");
+	}
+
+	public void OpenTV()
+	{
+		SceneManager.LoadScene("SceneTV");
+	}
+
+	public void OpenRocket()
+	{
+		SceneManager.LoadScene("SceneRocket");
+	}
+
+	public void OpenBase()
+	{
+		SceneManager.LoadScene("SceneBase");
+	}
+}
